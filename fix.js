df4649e1c232721aa8cae13a18aabe9dc2aedd59
Hemanth Surya <suryahemanth0@gmail.com>
2026-06-28 19:58:00 +0530

    cleanup temp scripts

--- fix.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-let content = fs.readFileSync('admin/index.html', 'utf8');
-
-// The agent might have accidentally output \${ instead of ${ in template literals. Let's fix that.
-content = content.replace(/\\\$\{/g, '${');
-
-fs.writeFileSync('admin/index.html', content, 'utf8');
-console.log('Fixed \\${ in admin/index.html');
